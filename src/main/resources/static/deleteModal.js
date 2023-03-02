97e2df2131ee76f6e9201f8df380ac397d162a0c
Alexei <alexeiak.dev@gmail.com>
2023-03-02 06:58:02 +0300

    fix: roles in delete modal window

--- src/main/resources/static/deleteModal.js
+++ src/main/resources/static/deleteModal.js
@@ -1,47 +1,43 @@
-const id_del = document.getElementById('id_del');
-const name_del = document.getElementById('name_del');
-const lastname_del = document.getElementById('lastname_del');
-const age_del = document.getElementById('age_del');
-const email_del = document.getElementById('email_del');
-const role_del = document.getElementById("delete-role");
-
-const deleteModal = document.getElementById("deleteModal");
-const closeDeleteButton = document.getElementById("closeDelete");
-
-const bsDeleteModal = new bootstrap.Modal(deleteModal);
 
 async function deleteModalData(id) {
-    const  urlForDel = '/admin/users/' + id;
-    let usersPageDel = await fetch(urlForDel);
+    const idField_delete = document.getElementById('id_del');
+    const nameField_delete = document.getElementById('name_del');
+    const lastNameField_delete = document.getElementById('lastname_del');
+    const ageField_delete = document.getElementById('age_del');
+    const emailField_delete = document.getElementById('email_del');
+    const roleField_delete = document.getElementById("delete-role");
 
-    if (usersPageDel.ok) {
-        let userData =
-            await usersPageDel.json().then(user => {
-                id_del.value = `${user.id}`;
-                name_del.value = `${user.firstName}`;
-                lastname_del.value = `${user.lastName}`;
-                age_del.value = `${user.age}`;
-                email_del.value = `${user.email}`;
-                role_del.value = user.roles.map(r=>r.rolename).join(", ");
-            });
+    let user = await fetch('/admin/users/' + id).then(response => response.json());
 
-        bsDeleteModal.show();
-    } else {
-        alert(`Error, ${usersPageDel.status}`);
-    }
+    idField_delete.value = `${user.id}`;
+    nameField_delete.value = `${user.firstName}`;
+    lastNameField_delete.value = `${user.lastName}`;
+    ageField_delete.value = `${user.age}`;
+    emailField_delete.value = `${user.email}`;
+
+    Array.from(roleField_delete.options).forEach(option => {
+        option.selected = user.roles.some(role => role.id.toString() === option.value);
+    });
+
+    const deleteModalWindow = document.getElementById("deleteModal");
+    const newDeleteModalWindow = new bootstrap.Modal(deleteModalWindow);
+    newDeleteModalWindow.show();
+
 }
 
 async function deleteUser() {
-    let urlDel = '/admin/users/' + id_del.value;
-    let method = {
+    let deleteUser = {
         method: 'DELETE',
         headers: {
             "Content-Type": "application/json"
         }
     };
 
-    fetch(urlDel, method).then(() => {
-        closeDeleteButton.click();
+    const btn_closeDeleteModalWindow = document.getElementById("closeDelete");
+    let urlDel = '/admin/users/' + idField_delete.value;
+
+    fetch(urlDel, deleteUser).then(() => {
+        btn_closeDeleteModalWindow.click();
         getAdminPage();
     })
 }
